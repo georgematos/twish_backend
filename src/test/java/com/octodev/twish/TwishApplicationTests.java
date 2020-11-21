5df9f5216f5df25e98791336ea44428267bfacbc
George Matos <georgemattos@gmail.com>
2020-11-21 06:10:24 -0300

    Start do projeto

---
+++ src/test/java/com/octodev/twish/TwishApplicationTests.java
@@ -0,0 +1,13 @@
+package com.octodev.twish;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TwishApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
