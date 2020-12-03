e125e1b5bf7211d3232d1cc14db3916cdae8a061
George Matos <georgemattos@gmail.com>
2020-12-03 17:30:33 -0300

    concecao de permissao de acesso ao endpoint de todos para admin e usuarios

--- src/main/java/com/octodev/twish/controllers/TodoController.java
+++ src/main/java/com/octodev/twish/controllers/TodoController.java
@@ -13,6 +13,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -25,6 +26,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 @RestController
+@PreAuthorize("hasRole('USER') or hasRole('ADMIN')")
 @RequestMapping("/todo")
 public class TodoController {
 
